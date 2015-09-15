e861cb92a472f8711c69a8e98f6e66293713c60c
Joe Rocklin <joe.rocklin@gmail.com>
2015-09-15 08:58:38 -0400

    Add hello world c program and info on HLL

---
+++ lesson_01/code/hello_world.c
@@ -0,0 +1,9 @@
+// Compile with either:
+//   clang hello_world.c -o hello-world-c
+//        or
+//   gcc hello_world.c -o hello-world-c
+#include <stdio.h>
+
+int main() {
+  printf("Hello, World!\n");
+}
